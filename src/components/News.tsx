52f90bf16e800259b1ed7660c4f05181e1fe5942
Sabir Hassan <sabirbk06@gmail.com>
2022-09-19 02:31:23 +0500

    chore: improve UX

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react"
 import Container from "react-bootstrap/Container"
-// import Pagination from "react-bootstrap/Pagination"
 import Pagination from "@mui/material/Pagination"
-
 import NewsItem from "./NewsItem"
-import { useLoading } from "../hooks"
+import Loading from "./Loading"
 
 const PAGE_SIZE = 9
 
 const News = () => {
-  const { Loading, setIsLoading } = useLoading()
+  const [isLoading, setIsLoading] = useState(true)
   const [articles, setArticles] = useState<any>([])
   const [totalArticles, setTotalArticles] = useState(0)
   const [page, setPage] = useState(1)
@@ -34,22 +32,26 @@ const News = () => {
 
   return (
     <Container>
-      <h3>Top Headlines</h3>
-      <Loading />
-      <div className="row">
-        {articles.map((items: any, id: number) => (
-          <div className="col-sm-12 col-md-6 col-lg-4" key={items.url}>
-            <NewsItem
-              title={items.title.slice(0, 45) + ".."}
-              description={items.description?.slice(0, 85) + "..."}
-              imageURL={items.urlToImage}
-              newsURL={items.url}
-            />
-          </div>
-        ))}
-      </div>
+      <h3 className="text-center">Top Headlines</h3>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="row">
+          {articles.map((items: any, id: number) => (
+            <div className="col-sm-12 col-md-6 col-lg-4" key={items.url}>
+              <NewsItem
+                title={items.title.slice(0, 45) + ".."}
+                description={items.description?.slice(0, 85) + "..."}
+                imageURL={items.urlToImage}
+                newsURL={items.url}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+
       <Pagination
-        sx={{ marginBottom: "15px" }}
+        sx={{ display: "flex", justifyContent: "center", marginBottom: "15px" }}
         count={Math.floor(totalArticles / PAGE_SIZE)}
         onChange={(e, val) => setPage(val)}
         variant="outlined"
