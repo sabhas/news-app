1296f132a9542bb5c742679dfd0fd85fb2fbaf34
Sabir Hassan <sabirbk06@gmail.com>
2022-09-19 02:16:40 +0500

    feat: add pagination

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,20 +1,36 @@
 import { useEffect, useState } from "react"
 import Container from "react-bootstrap/Container"
+// import Pagination from "react-bootstrap/Pagination"
+import Pagination from "@mui/material/Pagination"
+
 import NewsItem from "./NewsItem"
 import { useLoading } from "../hooks"
+
+const PAGE_SIZE = 9
 
 const News = () => {
   const { Loading, setIsLoading } = useLoading()
   const [articles, setArticles] = useState<any>([])
+  const [totalArticles, setTotalArticles] = useState(0)
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
-    const url = `${process.env.REACT_APP_NEWS_API_URL}?apiKey=${process.env.REACT_APP_NEWS_API_KEY}&country=us`
+    window.scrollTo(0, 0) //scrolls to top.
+    setIsLoading(true)
+
+    const url = `${process.env.REACT_APP_NEWS_API_URL}?apiKey=${
+      process.env.REACT_APP_NEWS_API_KEY
+    }&country=us&page=${page + 1}&pageSize=${PAGE_SIZE}`
+
     fetch(url)
       .then((data) => data.json())
-      .then((data) => setArticles(data.articles))
+      .then((data) => {
+        setArticles(data.articles)
+        setTotalArticles(data.totalResults)
+      })
       .catch((err) => console.log(err))
       .finally(() => setIsLoading(false))
-  }, [setIsLoading])
+  }, [setIsLoading, page])
 
   return (
     <Container>
@@ -32,6 +48,15 @@ const News = () => {
           </div>
         ))}
       </div>
+      <Pagination
+        sx={{ marginBottom: "15px" }}
+        count={Math.floor(totalArticles / PAGE_SIZE)}
+        onChange={(e, val) => setPage(val)}
+        variant="outlined"
+        shape="rounded"
+        showFirstButton
+        showLastButton
+      />
     </Container>
   )
 }
